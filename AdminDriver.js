7d40aa5122b2a380d24687fcb554cbd823c773f9
Eisen Montalvo <eisen.montalvo@rtx.com>
2021-06-19 15:11:48 +0000

    Implemented double elimination

--- AdminDriver.js
+++ AdminDriver.js
@@ -1,15 +1,46 @@
 const io = require("socket.io-client")
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers');
+const { exit } = require('yargs');
+
+const argv = yargs(hideBin(process.argv)).argv
 
 const URL = "http://engine.rmd-hackathon.xyz:8080";
 const socket = io(URL, { reconnection: true});
 
 socket.on("ready", () => {
-    console.log("Start tournament")
-    socket.emit("start", "round robin")
+    if(argv.rr === true)
+    {
+        console.log("Start round robin tournament")
+        socket.emit("start", {
+            type: "round robin"
+        })
+    }
+    else if(argv.de === true)
+    {
+        console.log("Start double elimination tournament")
+        socket.emit("start", {
+            type: "double elimination", 
+            seeds: [
+                {
+                    player1: 'eisen1',
+                    player2: 'by'
+                },
+                {
+                    player1: 'eisen2',
+                    player2: 'by'
+                },
+                {
+                    player1: 'eisen3',
+                    player2: 'eisen4'
+                }
+            ]
+        })
+    }
 })
 
 socket.on('connect', function (sock) {
-    console.log('Admin connected!')
+    console.log('Admin connection accepted!')
     socket.emit("set admin", socket.id)
 })
 
--- main.js
+++ main.js
@@ -38,38 +38,36 @@ const START_BOARD = [
 ]
 
 const validIPs = [
-"18.144.50.111",
-"54.177.207.33",
-"13.56.12.96",
-"54.67.47.117",
-"52.53.130.123",
-"54.215.180.67",
-"18.144.56.8",
-"54.219.97.244",
-"54.67.92.131",
-"54.183.27.25",
-"18.144.15.199",
-"3.101.80.163",
-"54.153.106.236",
-"52.53.230.44",
+    "54.67.123.203",
+    "18.144.50.111",
+    "54.177.207.33",
+    "13.56.12.96",
+    "54.67.47.117",
+    "52.53.130.123",
+    "54.215.180.67",
+    "18.144.56.8",
+    "54.219.97.244",
+    "54.67.92.131",
+    "54.183.27.25",
+    "18.144.15.199",
+    "3.101.80.163",
+    "54.153.106.236",
+    "52.53.230.44",
 ]
 
 var admin = NIL
 
 let connected = 0
 
-function ToString(arrayBoard)
-{
+function ToString(arrayBoard) {
     var strBoard = ""
-    for(let cell of arrayBoard)
-    {
+    for (let cell of arrayBoard) {
         strBoard += cell.toString()
     }
     return strBoard
 }
 
-function ToArray(strBoard)
-{
+function ToArray(strBoard) {
     let arrayBoard = [
         0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0,
@@ -81,21 +79,19 @@ function ToArray(strBoard)
         0, 0, 0, 0, 0, 0, 0, 0
     ]
 
-    for (var idx = 0; idx < strBoard.length; idx++)
-    {
+    for (var idx = 0; idx < strBoard.length; idx++) {
         arrayBoard[idx] = parseInt(strBoard.charAt(idx))
     }
 
     return arrayBoard
 }
 
-function connection(socket)
-{
+function connection(socket) {
     let connection = socket.request.connection._peername
     let ip = connection.address.split(":")[3];
-    console.log('connection :', ip);
-    
-    if(tournament_running || validIPs.includes(ip) === false) {
+    console.log('Connection from:', ip);
+
+    if (tournament_running ) { //|| validIPs.includes(ip) === false) {
         socket.disconnect(true)
         return
     }
@@ -104,8 +100,7 @@ function connection(socket)
 
     socket.on("set team", (data) => {
         var name = data["name"]
-        if(name == "")
-        {
+        if (name == "") {
             socket.disconnect(true)
             return
         }
@@ -125,22 +120,19 @@ function connection(socket)
         sockets[socket.id].on("move", move)
         sockets[socket.id].on("pass", pass)
         sockets[socket.id].on("disconnecting", () => {
-            idx = clients.findIndex( (client) => {
+            idx = clients.findIndex((client) => {
                 return client.id === socket.id
             })
             console.log(clients[idx].name + " disconnected!")
-            if(tournament_running)
-            {
+            if (tournament_running) {
                 clients[idx].disconnected = true
 
                 games.find((game) => {
-                    if(game.black.id === socket.id || game.white.id === socket.id) {
-                        if(game.black.id === socket.id)
-                        {
+                    if (game.black.id === socket.id || game.white.id === socket.id) {
+                        if (game.black.id === socket.id) {
                             game.board = ToArray("2000200020002000200020002000200020002000200020002000200020002000")
                         }
-                        else if(game.white.id === socket.id)
-                        {
+                        else if (game.white.id === socket.id) {
                             game.board = ToArray("1000100010001000100010001000100010001000100010001000100010001000")
                         }
                         game.ended = true
@@ -155,33 +147,30 @@ function connection(socket)
     socket.on("set admin", set_admin)
 }
 
-function start(type)
-{
-    console.log("Start tournament of type:", type)
-    if(tournament_running == true)
-    {
+function start(data) {
+    if (tournament_running == true) {
         console.log("Tournament running, can't start a new one")
         return
     }
-
-    var data = {
+    
+    var dataOut = {
         timeout: timeout
     }
-    for(let client of clients)
-    {
-        client.socket.emit("set timeout", data)
+    for (let client of clients) {
+        client.socket.emit("set timeout", dataOut)
     }
 
-    if(type === "round robin")
-    {
+    const type = data['type']
+    console.log("Start tournament of type:", type)
+
+    if (type === "round robin") {
         tournament_running = true
 
-        for(var idx = 0; idx < clients.length; ++idx )
-        {
-            if(clients[idx].disconnected) continue
-            for(var jdx = idx + 1; jdx < clients.length; ++jdx ) // Everybody plays everybody once
+        for (var idx = 0; idx < clients.length; ++idx) {
+            if (clients[idx].disconnected) continue
+            for (var jdx = idx + 1; jdx < clients.length; ++jdx) // Everybody plays everybody once
             {
-                if(clients[jdx].disconnected) continue
+                if (clients[jdx].disconnected) continue
                 var game = {
                     id: uuidv4(),
                     client1: clients[idx],
@@ -209,7 +198,7 @@ function start(type)
                 ++game.client2.game_count
 
                 // Determine white player
-                if(game.client1 === game.black) game.white = game.client2
+                if (game.client1 === game.black) game.white = game.client2
                 else game.white = game.client1
 
                 games.push(game)
@@ -218,21 +207,68 @@ function start(type)
 
         launch_games()
     }
-    else if(type === "double elimination")
-    {
+    else if (type === "double elimination") {
+        tournament_running = true
+        const seeds = data['seeds']
 
+        games = []
+        for (var idx = 0; idx < seeds.length; ++idx) {
+
+            const player1 = clients.find((client) => {
+                return client.name === seeds[idx].player1
+            })
+
+            const player2 = clients.find((client) => {
+                return client.name === seeds[idx].player2
+            })
+
+            if ( typeof player2 === "undefined") {
+                // This is a by
+                continue
+            }
+
+            var game = {
+                id: uuidv4(),
+                client1: player1,
+                client2: player2,
+                black: Math.random() < 0.5 ? player1 : player2,
+                white: NIL, // Need to determine below
+                turn: BLACK,
+                board: START_BOARD,
+                pass_count: 0,
+                ended: false,
+                pending: true,
+                watchdog: -1
+            }
+
+            // Set Opponents
+            var opponent = {
+                game_id: game.id,
+                name: game.client2.name
+            }
+            game.client1.socket.emit("set opponent", opponent)
+            opponent.name = game.client1.name
+            game.client2.socket.emit("set opponent", opponent)
+
+            ++game.client1.game_count
+            ++game.client2.game_count
+
+            // Determine white player
+            if (game.client1 === game.black) game.white = game.client2
+            else game.white = game.client1
+
+            games.push(game)
+        }
+
+        launch_games()
     }
 }
 
-function launch_games()
-{
-    for( let game of games )
-    {
-        if(game.ended === false)
-        {
-            if(game.black.busy === false && 
-                game.white.busy === false)
-            {
+function launch_games() {
+    for (let game of games) {
+        if (game.ended === false) {
+            if (game.black.busy === false &&
+                game.white.busy === false) {
                 game.pending = false
                 game.black.busy = true
                 game.white.busy = true
@@ -257,19 +293,16 @@ function start_watchdog(game) {
     }, 10 * 60 * 1000)
 }
 
-function get_game_idx(game_id)
-{
+function get_game_idx(game_id) {
     var idx = 0
-    for(let game of games)
-    {
-        if(game.id === game_id) return idx
+    for (let game of games) {
+        if (game.id === game_id) return idx
         ++idx
     }
     return -1
 }
 
-function move(data)
-{
+function move(data) {
     var game_id = data["game_id"]
     var game_idx = get_game_idx(game_id)
 
@@ -281,102 +314,86 @@ function move(data)
     next_turn(games[game_idx])
 }
 
-function pass(data)
-{
+function pass(data) {
     var game_id = data["game_id"]
     var game_idx = get_game_idx(game_id)
 
     games[game_idx].pass_count += 1
     clearTimeout(games[game_idx].watchdog)
-    if(games[game_idx].pass_count < 2)
-    {
+    if (games[game_idx].pass_count < 2) {
         next_turn(games[game_idx])
     }
-    else
-    {
+    else {
         // Game is a tie or no valid moves left, finish game
         games[game_idx].ended = true
         calculate_scores(games[game_idx])
     }
 }
 
-function next_turn(game)
-{
-    if(game_ended(game) === false)
-    {
+function next_turn(game) {
+    if (game_ended(game) === false) {
         var data = {
             game_id: game.id,
             board: ToString(game.board),
             turn: game.turn
         }
 
-        if(game.turn === BLACK)
-        {
+        if (game.turn === BLACK) {
             game.turn = WHITE
             data.turn = game.turn
             game.white.socket.emit("make move", data)
         }
-        else
-        {
+        else {
             game.turn = BLACK
             data.turn = game.turn
             game.black.socket.emit("make move", data)
         }
         start_watchdog(game)
     }
-    else
-    {
+    else {
         // Game ended
         calculate_scores(game)
     }
 }
 
-function game_ended(game)
-{
+function game_ended(game) {
     var empty_cells = 64;
-    for( let cell of game.board)
-    {
-        if(cell === 0) break
+    for (let cell of game.board) {
+        if (cell === 0) break
         --empty_cells
     }
 
-    if(empty_cells === 0)
-    {
+    if (empty_cells === 0) {
         game.ended = true
     }
 
     return game.ended
 }
 
-function calculate_scores(game)
-{
+function calculate_scores(game) {
     var data = {
         game_id: game.id,
         black_count: 0,
         white_count: 0
     }
 
-    for(let cell of game.board)
-    {
-        if(cell === 1) ++data.black_count
-        else if(cell === 2) ++data.white_count
+    for (let cell of game.board) {
+        if (cell === 1) ++data.black_count
+        else if (cell === 2) ++data.white_count
     }
 
     game.client1.socket.emit("game ended", data)
     game.client2.socket.emit("game ended", data)
 
-    if(data.black_count > data.white_count)
-    {
+    if (data.black_count > data.white_count) {
         ++game.black.win_count
         ++game.white.lost_count
     }
-    else if(data.white_count > data.black_count)
-    {
+    else if (data.white_count > data.black_count) {
         ++game.white.win_count
         ++game.black.lost_count
     }
-    else
-    {
+    else {
         ++game.black.tie_count
         ++game.white.tie_count
     }
@@ -388,24 +405,20 @@ function calculate_scores(game)
     tournament_ended()
 }
 
-function tournament_ended()
-{
+function tournament_ended() {
     var games_ended = 0
-    for(let game of games)
-    {
-        if(game.ended === true)
-        {
+    for (let game of games) {
+        if (game.ended === true) {
             ++games_ended
         }
     }
 
-    if(games_ended === games.length) // Tournament complete
+    if (games_ended === games.length) // Tournament complete
     {
         tournament_running = false
         console.log("Results: name | game count: | win count | loss count | tie count ")
-        for( let client of clients)
-        {
-            console.log(client.name, client.game_count,":  " , client.win_count, client.lost_count, client.tie_count)
+        for (let client of clients) {
+            console.log(client.name, client.game_count, ":  ", client.win_count, client.lost_count, client.tie_count)
             var data = {
                 name: client.name,
                 game_count: client.game_count,
@@ -417,8 +430,7 @@ function tournament_ended()
     }
 }
 
-function set_admin(socket_id)
-{
+function set_admin(socket_id) {
     console.log("Setting admin")
     var socket = sockets[socket_id]
 
@@ -434,11 +446,9 @@ io.on("start", start)
 // Viewers namespace
 const viewersNamespace = io.of("/viewers")
 
-function sendGameStatus(socket, id)
-{
+function sendGameStatus(socket, id) {
     let idx = get_game_idx(id)
-    if( typeof games[idx] != 'undefined')
-    {
+    if (typeof games[idx] != 'undefined') {
         socket.emit("game-board", {
             id: games[idx].id,
             board: ToString(games[idx].board),
@@ -462,10 +472,9 @@ viewersNamespace.on("connection", (socket) => {
     })
 });
 
-setInterval( () => {
+setInterval(() => {
     let games_data = []
-    for( let game of games )
-    {
+    for (let game of games) {
         // Kickoff each game
         var game_data = {
             id: game.id,
@@ -485,17 +494,17 @@ setInterval(() => {
     let participants = []
 
     clients.forEach(client => {
-            let participant = {
-                id: client.id,
-                name: client.name,
-                game_count: client.game_count,
-                win_count: client.win_count,
-                lost_count: client.lost_count,
-                tie_count: client.tie_count,
-                disconnected: client.disconnected
-            }
+        let participant = {
+            id: client.id,
+            name: client.name,
+            game_count: client.game_count,
+            win_count: client.win_count,
+            lost_count: client.lost_count,
+            tie_count: client.tie_count,
+            disconnected: client.disconnected
+        }
 
-            participants.push(participant)
+        participants.push(participant)
     });
     viewersNamespace.emit("participants-list", participants)
 }, 5000)
